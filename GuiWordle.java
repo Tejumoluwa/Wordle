fc918373606a74a9fcf426ae8aeb442231d4807d
Tejumoluwa <77273561+Tejumoluwa@users.noreply.github.com>
2024-03-14 10:46:34 +0100

    New changes

--- GuiWordle.java
+++ GuiWordle.java
@@ -2,12 +2,24 @@ package Wordle;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.EventListener;
+import java.util.Random;
 
-public class GuiWordle extends JFrame {
+public class GuiWordle extends JFrame implements ActionListener {
     private JPanel word_guessed, guessing, keypad, key1, key2, key3, notify, center;
-    private JLabel wordle, clues, words;
-    private JButton a, b, c, d, e,f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, enter;
-    private JTextField word;
+    private JLabel wordle, clues, words, word, guess1, guess2, guess3, guess4, guess5, guess6;
+    private JButton a, b, c, d, e,f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, enter, delete;
+
+    String[] arr_words = {"CABIN", "EAGER", "HABIT", "LABOR", "SOBER"};
+    StringBuffer str_word;
+    int lives = 6, rnd, index =0;
+    Random rd;
+    int begin = 0;
+    String hiddenWord, word2 = "";
+    ArrayList<JLabel> arr;
 
     public GuiWordle(){
         center = new JPanel();
@@ -22,16 +34,16 @@ public class GuiWordle extends JFrame {
 //        guessing.setBackground(new Color(255, 0, 0));
         GridBagConstraints gc1 = new GridBagConstraints();
         guessing.setLayout(new GridBagLayout());
-        clues = new JLabel("Clues");
+        clues = new JLabel();
         clues.setFont(new Font("serif", Font.BOLD, 25));
         gc1.gridx = 5;
         gc1.gridy = 5;
         guessing.add(clues, gc1);
         clues.setSize(100,0);
-        word = new JTextField(10);
+        word = new JLabel();
         word.setFont(new Font("Serif", Font.BOLD, 25));
         gc1.gridx = 5;
-        gc1.gridy = 6;
+        gc1.gridy = 7;
         guessing.add(word, gc1);
 
         a = new JButton("A");
@@ -61,6 +73,7 @@ public class GuiWordle extends JFrame {
         y = new JButton("Y");
         z = new JButton("Z");
         enter = new JButton("ENTER");
+        delete = new JButton("DELETE");
 
         keypad = new JPanel();
         GridBagConstraints gc = new GridBagConstraints();
@@ -103,6 +116,7 @@ public class GuiWordle extends JFrame {
 
         key3 = new JPanel();
         key3.setLayout(new FlowLayout());
+        key3.add(delete);
         key3.add(z);
         key3.add(x);
         key3.add(c);
@@ -116,15 +130,45 @@ public class GuiWordle extends JFrame {
         gc.gridy = 2;
         keypad.add(key3, gc);
 
+        arr = new ArrayList<>();
+        guess1 = new JLabel();
+        guess1.setFont(new Font("Serif", Font.BOLD, 18));
+        guess2 = new JLabel();
+        guess2.setFont(new Font("Serif", Font.BOLD, 18));
+        guess3 = new JLabel();
+        guess3.setFont(new Font("Serif", Font.BOLD, 18));
+        guess4 = new JLabel();
+        guess4.setFont(new Font("Serif", Font.BOLD, 18));
+        guess5 = new JLabel();
+        guess5.setFont(new Font("Serif", Font.BOLD, 18));
+        guess6 = new JLabel();
+        guess6.setFont(new Font("Serif", Font.BOLD, 18));
         word_guessed = new JPanel();
         words = new JLabel("Words Guessed");
         words.setFont(new Font("Serif", Font.BOLD, 25));
         word_guessed.add(words);
+        word_guessed.setLayout(new GridLayout(7,1));
+        word_guessed.add(guess1);
+        word_guessed.add(guess2);
+        word_guessed.add(guess3);
+        word_guessed.add(guess4);
+        word_guessed.add(guess5);
+        word_guessed.add(guess6);
 
+        rd = new Random();
+        rnd = rd.nextInt(5);
+        str_word = new StringBuffer();
+        hiddenWord = arr_words[rnd];
 
         notify.add(wordle);
         center.add(word_guessed);
         center.add(guessing);
+        arr.add(guess1);
+        arr.add(guess2);
+        arr.add(guess3);
+        arr.add(guess4);
+        arr.add(guess5);
+        arr.add(guess6);
         setLayout(new BorderLayout());
         add(notify, BorderLayout.NORTH);
         add(keypad, BorderLayout.SOUTH);
@@ -132,12 +176,105 @@ public class GuiWordle extends JFrame {
         setSize(800, 650);
         setVisible(true);
 
+        a.addActionListener(this);
+        b.addActionListener(this);
+        c.addActionListener(this);
+        d.addActionListener(this);
+        e.addActionListener(this);
+        f.addActionListener(this);
+        g.addActionListener(this);
+        h.addActionListener(this);
+        i.addActionListener(this);
+        j.addActionListener(this);
+        k.addActionListener(this);
+        l.addActionListener(this);
+        m.addActionListener(this);
+        n.addActionListener(this);
+        o.addActionListener(this);
+        p.addActionListener(this);
+        q.addActionListener(this);
+        r.addActionListener(this);
+        s.addActionListener(this);
+        t.addActionListener(this);
+        u.addActionListener(this);
+        v.addActionListener(this);
+        w.addActionListener(this);
+        x.addActionListener(this);
+        y.addActionListener(this);
+        z.addActionListener(this);
+        enter.addActionListener(this);
+        delete.addActionListener(this);
+        enter.setEnabled(false);
+        delete.setEnabled(false);
         setTitle("Wordle");
         setDefaultCloseOperation(3);
         setLocationRelativeTo(null);
     }
 
-    public static void main(String[] args) {
-        new GuiWordle();
+    public void playGame(){
+        for(int i = 0; i < 5; i++){
+            str_word.append("_  ");
+        }
+        word.setText(str_word.toString());
+        System.out.println(hiddenWord);
     }
+
+    public void actionPerformed(ActionEvent e) {
+        String letter = e.getActionCommand();
+        delete.setEnabled(true);
+        if(e.getActionCommand().equals("DELETE")){
+            begin--;
+            str_word.replace((begin*3), ((begin * 3) + 1), "_");
+        } else if (e.getActionCommand().equals("ENTER")) {
+            String str = str_word.toString();
+            str = str.replace(" ", "");
+            System.out.println(str.length());
+            if (hiddenWord.equals(str)) {
+                JOptionPane.showMessageDialog(this, "Congrats you guessed the word");
+                JOptionPane.showConfirmDialog(this, "Do you want to exit the game","OPTION", JOptionPane.YES_NO_OPTION);
+                if(JOptionPane.YES_OPTION == 0){
+                    System.exit(0);
+                }else{
+                    GuiWordle wordle = new GuiWordle();
+                    wordle.playGame();
+                }
+            } else {
+                for (int j = 0; j < str.length(); j++) {
+                    if (hiddenWord.charAt(j) == str.charAt(j)) {
+                        word2 += "*  ";
+                    } else if (hiddenWord.indexOf(str.charAt(j)) != -1) {
+                        word2 += "+  ";
+                    } else {
+                        word2 += "x  ";
+                    }
+                }
+                clues.setText(word2);
+                arr.get(index).setText(str);
+                index++;
+                try{Thread.sleep(500);}catch (Exception o){}
+                str_word = new StringBuffer("");
+                word2 = "";
+                begin = 0;
+                enter.setEnabled(false);
+                playGame();
+            }
+        } else {
+            if (str_word.length() == 15 && begin < 5) {
+                str_word.replace((begin * 3), ((begin * 3) + 1), letter);
+                begin++;
+            }
+
+        }
+        word.setText(str_word.toString());
+        if (begin == 5) {
+            enter.setEnabled(true);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        GuiWordle wordle = new GuiWordle();
+        wordle.playGame();
+    }
+
 }
---
+++ Menu.java
@@ -0,0 +1,111 @@
+package Wordle;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JFrame implements ActionListener {
+    JPanel panel1, panel2;
+    JLabel wordle, hint;
+    JButton play, vi, exit;
+
+    public Menu(){
+        Color color = new Color(174, 55, 255);
+        panel1 = new JPanel();
+        panel1.setBackground(color);
+        panel2 = new JPanel();
+        panel2.setBackground(color);
+        wordle = new JLabel("Wordle");
+        wordle.setFont(new Font("Arial", Font.BOLD, 25));
+        wordle.setForeground(Color.black);
+        hint = new JLabel("Get 6 chances to  guess a 5-letter word");
+        hint.setForeground(Color.BLACK);
+        hint.setFont(new Font("Arial", Font.ITALIC, 18));
+        play = new JButton("Play");
+        play.setFont(new Font("Arial", Font.PLAIN, 16));
+        play.setFocusable(false);
+        play.setBackground(Color.gray);
+        play.setForeground(Color.white);
+        vi = new JButton("View Instructions");
+        vi.setFont(new Font("Arial", Font.PLAIN, 16));
+        vi.setFocusable(false);
+        vi.setBackground(Color.gray);
+        vi.setForeground(Color.white);
+        exit = new JButton("Exit");
+        exit.setFont(new Font("Arial", Font.PLAIN, 16));
+        exit.setFocusable(false);
+        exit.setBackground(Color.gray);
+        exit.setForeground(Color.white);
+
+
+        setDefaultCloseOperation(3);
+        setVisible(true);
+        setSize(350, 200);
+        setLocationRelativeTo(null);
+        setTitle("Wordle Menu");
+//        setResizable(false);
+
+        play.addActionListener(this);
+        vi.addActionListener(this);
+        exit.addActionListener(this);
+    }
+
+    public void addComponents(){
+        panel1.setLayout(new GridBagLayout());
+        GridBagConstraints c1 = new GridBagConstraints();
+
+        c1.gridx = 0;
+        c1.gridy = 0;
+        panel1.add(wordle, c1);
+
+        c1.gridx = 0;
+        c1.gridy = 1;
+        c1.weighty = 0.1;
+        panel1.add(hint, c1);
+
+
+        panel2.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+
+        c.gridx = 0;
+        c.gridy = 2;
+        c.fill = c.HORIZONTAL;
+        c.weighty = 0.1;
+        panel2.add(play, c);
+
+        c.gridx = 0;
+        c.gridy = 3;
+        c.fill = c.HORIZONTAL;
+        panel2.add(vi, c);
+
+        c.gridx = 0;
+        c.gridy = 4;
+        c.fill = c.HORIZONTAL;
+        panel2.add(exit, c);
+
+
+        setLayout(new BorderLayout());
+        add(panel1, BorderLayout.NORTH);
+        add(panel2, BorderLayout.CENTER);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        Wordle wordle = new Wordle();
+        if(e.getSource().equals(play)){
+            wordle.playGame();
+        }else if(e.getSource().equals(vi)){
+//            wordle.viewInstructions();
+        }else if(e.getSource().equals(exit)){
+            JOptionPane.showConfirmDialog(this,"Are you sure you want to exit", "Option", JOptionPane.YES_NO_OPTION);
+            if(JOptionPane.YES_OPTION == 0){
+                System.exit(0);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Menu mn = new Menu();
+        mn.addComponents();
+    }
+}
